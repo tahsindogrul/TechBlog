8c6da0ab9725c13684587e239c63ba2b75c5538a
tahsindogrul <tahsindogrul@icloud.com>
2024-08-18 00:51:33 +0300

    Comment modeli oluşturuldu

---
+++ TechBlog.Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }= DateTime.Now;
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+        public int AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+
+    }
+}
