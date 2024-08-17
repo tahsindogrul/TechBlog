7586fa64039600699ed1d4400d658c5c450757b6
tahsindogrul <tahsindogrul@icloud.com>
2024-08-17 23:34:11 +0300

    Admin ve User arealar oluşturuldu

---
+++ TechBlog.Web/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechBlog.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
