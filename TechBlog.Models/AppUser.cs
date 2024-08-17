4809a6c631fcfc5441b872547bb406713f64d510
tahsindogrul <tahsindogrul@icloud.com>
2024-08-18 00:51:09 +0300

    AppUser modeli oluşturuldu

---
+++ TechBlog.Models/AppUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? ProfilePicture { get; set; }
+        public bool IsAdmin { get; set; }=false;
+        public DateTime DateCreated { get; set; }= DateTime.Now;
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
+        public virtual ICollection<Post> Posts { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
+    }
+}
