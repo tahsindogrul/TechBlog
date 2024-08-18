d952ea0cd83b1f6c289bbafef97e926710865df9
tahsindogrul <tahsindogrul@icloud.com>
2024-08-18 22:44:51 +0300

    Program.csde ApplicationDbContext sınıfı için builder servisleri ayarlandı ve gerekli paketler install edildi

--- TechBlog.Web/Program.cs
+++ TechBlog.Web/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
+using TechBlog.Data;
+using TechBlog.Repository.Shared.Abstract;
+using TechBlog.Repository.Shared.Shared;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
