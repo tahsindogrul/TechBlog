d6c412be2021b7131817c000272fa1651d744034
tahsindogrul <tahsindogrul@icloud.com>
2024-08-18 23:49:09 +0300

    Business katmanında generic servis oluşturuldu

---
+++ TechBlog.Business/Shared/Abstract/IServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlog.Models;
+
+namespace TechBlog.Business.Shared.Abstract
+{
+    public interface IServices<T> where T : BaseModel
+    {
+        ICollection<T> GetAll();
+        T GetById(int id);
+        T Add(T entity);
+        T Update(T entity);
+        bool Delete(int id);
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        ICollection<T> GetAll(Expression<Func<T, bool>> predicate);
+
+    }
+}
---
+++ TechBlog.Business/Shared/Concrete/Services.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlog.Business.Shared.Abstract;
+using TechBlog.Models;
+using TechBlog.Repository.Shared.Abstract;
+
+namespace TechBlog.Business.Shared.Concrete
+{
+    public class Services<T> : IServices<T> where T : BaseModel
+    {
+        private readonly IRepository<T> _repository;
+
+        public Services(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public T Add(T entity)
+        {
+
+            return _repository.Add(entity);
+
+        }
+
+        public bool Delete(int id)
+        {
+            return _repository.Delete(id);
+           
+        }
+
+        public ICollection<T> GetAll()
+        {
+            return _repository.GetAll().ToList();  
+        }
+
+        public ICollection<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return _repository.GetAll(predicate).ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+           return _repository.GetFirstOrDefault(predicate);
+        }
+
+        public T Update(T entity)
+        {
+            return _repository.Update(entity);
+        }
+    }
+}
