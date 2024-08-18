6a11c72db93aa1f8699f1528aa2dbcb3d8cc3636
tahsindogrul <tahsindogrul@icloud.com>
2024-08-18 22:45:11 +0300

    Generic Repository oluşturuldu

---
+++ TechBlog.Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Repository.Shared.Abstract
+{
+    public interface IRepository<T> where T :class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T Update(T entity);
+        T Add(T entity);
+        void Delete(int id);
+        T GetById(int id);
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        void Save();
+    }
+}
---
+++ TechBlog.Repository/Shared/Shared/Repository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlog.Data;
+using TechBlog.Repository.Shared.Abstract;
+
+namespace TechBlog.Repository.Shared.Shared
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet=_context.Set<T>();
+           
+        }
+
+
+        public T Add(T entity)
+        {
+            _dbSet.Add(entity);
+            Save();
+            return entity;
+        }
+
+        public void Delete(int id)
+        {
+            var item= _dbSet.Find(id);
+            _dbSet.Remove(item);
+            Save();
+            return;
+          
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public T GetById(int id)
+        {
+           return _dbSet.Find(id);
+           
+
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+           return _dbSet.FirstOrDefault(predicate);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public T Update(T entity)
+        {
+            _dbSet.Update(entity);
+            Save();
+            return entity;
+        }
+    }
+}
