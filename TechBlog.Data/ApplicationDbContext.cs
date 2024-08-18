53892dda53a5e30e74c9c4ff5cded6e619f2e4c9
tahsindogrul <tahsindogrul@icloud.com>
2024-08-18 22:44:10 +0300

    ApplicationDbContext sınıfı oluşturuldu.

---
+++ TechBlog.Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlog.Models;
+
+
+namespace TechBlog.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
