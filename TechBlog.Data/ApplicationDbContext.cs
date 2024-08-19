f18b61e5d6bc51b7b3ad5e128115597a7c74c9fc
tahsindogrul <tahsindogrul@icloud.com>
2024-08-20 01:21:06 +0300

    kategori configuration çalışması için gerekli işlemler yapıldı

--- TechBlog.Data/ApplicationDbContext.cs
+++ TechBlog.Data/ApplicationDbContext.cs
@@ -2,23 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TechBlog.Data.Configurations;
 using TechBlog.Models;
 
 
 namespace TechBlog.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext()
         {
 
         }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<AppUser> Users { get; set; }
@@ -26,5 +28,15 @@ namespace TechBlog.Data
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Post> Posts { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+          
+        }
     }
+
+
 }
+
