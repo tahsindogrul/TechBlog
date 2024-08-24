f10c3d6c73565b6c57dc904086a2c18c16dacdeb
tahsindogrul <tahsindogrul@icloud.com>
2024-08-24 04:15:14 +0300

    CategoryPostViewModel oluşturuldu

---
+++ TechBlog.Models/ViewModels/CategoryPostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models.ViewModels
+{
+    public class CategoryPostViewModel
+    {
+        public Category Category { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
