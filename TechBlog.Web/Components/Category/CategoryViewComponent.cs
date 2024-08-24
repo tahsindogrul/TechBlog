54af7979e7874869858a1bebe85ee978fa550e0e
tahsindogrul <tahsindogrul@icloud.com>
2024-08-24 04:10:51 +0300

    CategoryViewComponent  Oluşturuldu

---
+++ TechBlog.Web/Components/Category/CategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechBlog.Business.Abstract;
+
+namespace TechBlog.Web.Components.Category
+{
+    public class CategoryViewComponent:ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorires = _categoryService.GetAll();
+            return View(categorires);
+        }
+    }
+}
