5448e1b7e04f1aa10a8bce2dae2c345de3249daa
tahsindogrul <tahsindogrul@icloud.com>
2024-08-24 04:05:07 +0300

    Dto Katmanı Oluşturulup içerisine LoginDTO oluşturuldu

---
+++ TechBlog.Dtos/UserDTOs/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Dtos.UserDTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRememberMe { get; set; }
+    }
+}
