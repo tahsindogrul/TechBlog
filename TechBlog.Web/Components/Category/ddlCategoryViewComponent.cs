a6a8c5b9d3dd7b6d9423108ad0e205e6c38f3dbc
tahsindogrul <tahsindogrul@icloud.com>
2024-08-26 03:49:24 +0300

    CategoryDropdownlist view componenti oluşturuldu ve layouta eklendi

---
+++ TechBlog.Web/Components/Category/ddlCategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechBlog.Business.Abstract;
+
+namespace TechBlog.Web.Components.Category
+{
+    public class ddlCategoryViewComponent:ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+  
+
+        public ddlCategoryViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryService.GetAll();
+            return View("CategoryDDL",categories);
+        }
+    }
+}
