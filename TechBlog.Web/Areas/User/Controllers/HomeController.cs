8e18b8e0652975ec80f22f5e5432f8d1c4297adc
tahsindogrul <tahsindogrul@icloud.com>
2024-08-27 21:59:43 +0300

    User Area authorize edildi

--- TechBlog.Web/Areas/User/Controllers/HomeController.cs
+++ TechBlog.Web/Areas/User/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TechBlog.Web.Areas.User.Controllers
 {
     [Area("User")]
-    public class HomeController : Controller
+	[Authorize(Roles = "User")]
+	public class HomeController : Controller
     {
         public IActionResult Index()
         {
