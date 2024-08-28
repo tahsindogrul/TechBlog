ad123b2dfb38f2f81375e47ee3c49fa0e79d9c87
tahsindogrul <tahsindogrul@icloud.com>
2024-08-29 02:24:07 +0300

    Profile Controllera Index actionu ve Profile Update Actionu oluşturuldu

---
+++ TechBlog.Web/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TechBlog.Business.Abstract;
+using TechBlog.Models;
+
+namespace TechBlog.Web.Areas.User.Controllers
+{
+    public class ProfileController : UserBaseController
+    {
+        private readonly IUserService _userService;
+
+        public ProfileController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            var id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var user = _userService.GetUserById(id);
+            return View(user);
+
+        }
+
+        [HttpPost]
+        public IActionResult ProfileUpdate(AppUser user)
+        {
+            return Ok(_userService.ProfileUpdate(user));
+        }
+
+    }
+}
