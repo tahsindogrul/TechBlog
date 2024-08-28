5d4273b01ed3ca7fc03c9af3e7f9e85abca00a55
tahsindogrul <tahsindogrul@icloud.com>
2024-08-29 02:20:16 +0300

    UserBaseController oluşturuldu

---
+++ TechBlog.Web/Areas/User/Controllers/UserBaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechBlog.Web.Areas.User.Controllers
+{
+	[Area("User")]
+	[Authorize(Roles = "User")]
+	public class UserBaseController : Controller
+	{
+
+
+
+		public UserBaseController()
+		{
+		} 
+    }
+}
