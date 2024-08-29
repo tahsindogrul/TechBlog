5bddf187dfaad0dfef4fdff5afa8fb4257770412
tahsindogrul <tahsindogrul@icloud.com>
2024-08-29 23:59:50 +0300

    Profile controllerde PostCreate ve AddPost actionları oluşturuldu

--- TechBlog.Web/Areas/User/Controllers/ProfileController.cs
+++ TechBlog.Web/Areas/User/Controllers/ProfileController.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.Claims;
 using TechBlog.Business.Abstract;
 using TechBlog.Models;
+using TechBlog.Models.ViewModels;
 
 namespace TechBlog.Web.Areas.User.Controllers
 {
     public class ProfileController : UserBaseController
     {
         private readonly IUserService _userService;
+        private readonly IPostService _postService;
+        private readonly ICategoryService _categoryService;
 
-        public ProfileController(IUserService userService)
+        public ProfileController(IUserService userService, IPostService postService, ICategoryService categoryService)
         {
             _userService = userService;
+            _postService = postService;
+            _categoryService = categoryService;
         }
 
         public IActionResult Index()
@@ -28,6 +34,46 @@ namespace TechBlog.Web.Areas.User.Controllers
         {
             return Ok(_userService.ProfileUpdate(user));
         }
+
+
+        [HttpGet]
+        public IActionResult PostCreate()
+        {
+            var categories = _categoryService.GetAll().ToList();
+            var model = new PostCreateViewModel
+            {
+                UserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)),
+                Categories= new SelectList(categories,"Id","Name")
+
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(PostCreateViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var post = new Post
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    UserId = model.UserId,
+                    DateCreated = DateTime.Now,
+                    CategoryId=model.SelectedCategoryId,
+                };
+                _postService.Add(post);
+                return Json(new { success = true, message = "Post başarıyla eklendi!" });
+
+            }
+            var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+            return Json(new { success = false, message = string.Join(", ", errors) });
+        }
+
+
+
+
 
     }
 }
