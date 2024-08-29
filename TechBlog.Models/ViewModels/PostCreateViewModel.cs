1d8182cb0bf0ef1ef551ee27b9406d8cf0808d53
tahsindogrul <tahsindogrul@icloud.com>
2024-08-29 23:59:05 +0300

    PostCreate ViewModel oluşturuldu

---
+++ TechBlog.Models/ViewModels/PostCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models.ViewModels
+{
+    public class PostCreateViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int UserId { get; set; }
+        public IEnumerable<SelectListItem>? Categories { get; set; }
+        public int SelectedCategoryId { get; set; }
+    }
+}
