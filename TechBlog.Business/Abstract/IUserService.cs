0c895df969411b4f3853746c45541c69eb98a5d9
tahsindogrul <tahsindogrul@icloud.com>
2024-09-23 02:49:49 +0300

    IUserService de Login ve Logout methodları oluşturuldu

--- TechBlog.Business/Abstract/IUserService.cs
+++ TechBlog.Business/Abstract/IUserService.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TechBlog.Business.Shared.Abstract;
+using TechBlog.Dtos.UserDTOs;
 using TechBlog.Models;
 
 namespace TechBlog.Business.Abstract
 {
     public interface IUserService:IService<AppUser>
     {
+        Task<LoginDTO> Login(LoginDTO loginDTO);
+
         AppUser CheckUser(AppUser appUser);
         AppUser GetUserById(int userId);
-
+        Task Logout();
         AppUser ProfileUpdate(AppUser appUser);
     }
 }
