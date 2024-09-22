ed9d2b8e4ea5c779db02b691799e13b6068cf68f
tahsindogrul <tahsindogrul@icloud.com>
2024-09-23 02:47:04 +0300

    LoginDTO ya Role propertysi eklendi

--- TechBlog.Dtos/UserDTOs/LoginDTO.cs
+++ TechBlog.Dtos/UserDTOs/LoginDTO.cs
@@ -11,5 +11,6 @@ namespace TechBlog.Dtos.UserDTOs
         public string Email { get; set; }
         public string Password { get; set; }
         public bool IsRememberMe { get; set; }
+        public string? Role {  get; set; }
     }
 }
