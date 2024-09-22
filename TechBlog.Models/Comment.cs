21aad8d69c97456f28d89d4081b94da49b5fd2c0
tahsindogrul <tahsindogrul@icloud.com>
2024-09-23 02:45:25 +0300

    Post ve AppUser nesneleri  projedeki ihtiyaçlar doğrultusunda nullable hale getirildi

--- TechBlog.Models/Comment.cs
+++ TechBlog.Models/Comment.cs
@@ -12,9 +12,9 @@ namespace TechBlog.Models
         public string Content { get; set; }
         public DateTime DateCreated { get; set; }= DateTime.Now;
         public int PostId { get; set; }
-        public virtual Post Post { get; set; }
+        public virtual Post? Post { get; set; }
         public int UserId { get; set; }
-        public virtual AppUser User { get; set; }
+        public virtual AppUser? User { get; set; }
 
     }
 }
