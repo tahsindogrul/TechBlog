e460d81e46db9778a1ce9fab79eaa3bf8dc81beb
tahsindogrul <tahsindogrul@icloud.com>
2024-09-23 02:50:22 +0300

    ICategoryServicede GetCategoryWithPosts methodu oluşturuldu.

--- TechBlog.Business/Abstract/ICategoryService.cs
+++ TechBlog.Business/Abstract/ICategoryService.cs
@@ -5,10 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using TechBlog.Business.Shared.Abstract;
 using TechBlog.Models;
+using TechBlog.Models.ViewModels;
 
 namespace TechBlog.Business.Abstract
 {
     public interface ICategoryService: IService<Category>
     {
+        CategoryPostViewModel GetCategoryWithPosts(int id );
     }
 }
