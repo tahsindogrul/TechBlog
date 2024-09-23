5e567102006be68810b815d71f3b245d995d1d93
tahsindogrul <tahsindogrul@icloud.com>
2024-09-23 03:07:42 +0300

    Posts ve PostEdit methodları yazıldı

--- TechBlog.Web/Areas/User/Controllers/ProfileController.cs
+++ TechBlog.Web/Areas/User/Controllers/ProfileController.cs
@@ -7,73 +7,128 @@ using TechBlog.Models.ViewModels;
 
 namespace TechBlog.Web.Areas.User.Controllers
 {
-    public class ProfileController : UserBaseController
-    {
-        private readonly IUserService _userService;
-        private readonly IPostService _postService;
-        private readonly ICategoryService _categoryService;
+	public class ProfileController : UserBaseController
+	{
+		private readonly IUserService _userService;
+		private readonly IPostService _postService;
+		private readonly ICategoryService _categoryService;
 
-        public ProfileController(IUserService userService, IPostService postService, ICategoryService categoryService)
+		public ProfileController(IUserService userService, IPostService postService, ICategoryService categoryService)
+		{
+			_userService = userService;
+			_postService = postService;
+			_categoryService = categoryService;
+		}
+
+		public IActionResult Index()
+		{
+			var id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+			var user = _userService.GetUserById(id);
+			return View(user);
+
+		}
+
+		[HttpPost]
+		public IActionResult ProfileUpdate(AppUser user)
+		{
+			return Ok(_userService.ProfileUpdate(user));
+		}
+
+
+		[HttpGet]
+		public IActionResult PostCreate()
+		{
+			var categories = _categoryService.GetAll().ToList();
+			var model = new PostCreateViewModel
+			{
+				UserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)),
+				Categories = new SelectList(categories, "Id", "Name")
+
+			};
+			return View(model);
+		}
+
+		[HttpPost]
+		public IActionResult AddPost(PostCreateViewModel model)
+		{
+
+			if (ModelState.IsValid)
+			{
+				var post = new Post
+				{
+					Title = model.Title,
+					Content = model.Content,
+					UserId = model.UserId,
+					DateCreated = DateTime.Now,
+					CategoryId = model.SelectedCategoryId,
+				};
+				_postService.Add(post);
+				return Json(new { success = true, message = "Post başarıyla eklendi!" });
+
+			}
+			var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+			return Json(new { success = false, message = string.Join(", ", errors) });
+		}
+
+		public IActionResult Posts()
+		{
+			var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+			var posts = _postService.GetPostsByUserId(userId);
+			return View(posts);
+
+		}
+
+		[HttpGet]
+		public IActionResult PostEdit(int id)
         {
-            _userService = userService;
-            _postService = postService;
-            _categoryService = categoryService;
-        }
+            var post = _postService.GetById(id);
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-        public IActionResult Index()
-        {
-            var id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
-
-            var user = _userService.GetUserById(id);
-            return View(user);
-
-        }
-
-        [HttpPost]
-        public IActionResult ProfileUpdate(AppUser user)
-        {
-            return Ok(_userService.ProfileUpdate(user));
-        }
-
-
-        [HttpGet]
-        public IActionResult PostCreate()
-        {
-            var categories = _categoryService.GetAll().ToList();
-            var model = new PostCreateViewModel
+            if (post == null || post.UserId.ToString() != userId) // Kullanıcı yetkilendirme kontrolü
             {
-                UserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)),
-                Categories= new SelectList(categories,"Id","Name")
+                return NotFound();
+            }
 
+            var viewModel = new PostEditViewModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Content = post.Content,
+                CategoryId = post.CategoryId,
+                Categories = _categoryService.GetAll()
             };
-            return View(model);
+            return View(viewModel);
         }
 
-        [HttpPost]
-        public IActionResult AddPost(PostCreateViewModel model)
-        {
-
+		[HttpPost] 
+		public IActionResult PostEdit(PostEditViewModel viewModel)
+		{
             if (ModelState.IsValid)
             {
-                var post = new Post
+                var post = _postService.GetById(viewModel.Id);
+                if (post == null)
                 {
-                    Title = model.Title,
-                    Content = model.Content,
-                    UserId = model.UserId,
-                    DateCreated = DateTime.Now,
-                    CategoryId=model.SelectedCategoryId,
-                };
-                _postService.Add(post);
-                return Json(new { success = true, message = "Post başarıyla eklendi!" });
+                    return NotFound();
+                }
 
+                post.Title = viewModel.Title;
+                post.Content = viewModel.Content;
+                post.CategoryId = viewModel.CategoryId;
+                post.DateUpdated = DateTime.Now;
+
+                _postService.Update(post);
+                return Redirect("/User/Profile/Posts");
             }
-            var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
-            return Json(new { success = false, message = string.Join(", ", errors) });
+
+          
+            viewModel.Categories = _categoryService.GetAll();
+            return View(viewModel);
         }
 
 
 
 
 
-    }
+	}
 }
