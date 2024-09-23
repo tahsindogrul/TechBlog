fc25026af0d166a65c71367dcc603e6821957f06
tahsindogrul <tahsindogrul@icloud.com>
2024-09-23 03:00:22 +0300

    Admin Areada BlogPost Controller oluşturuldu ve gerekli actionlar yazıldı

---
+++ TechBlog.Web/Areas/Admin/Controllers/BlogPostController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechBlog.Business.Abstract;
+using TechBlog.Models;
+
+namespace TechBlog.Web.Areas.Admin.Controllers
+{
+    public class BlogPostController : AdminBaseController
+    {
+        private readonly IPostService _postService;
+
+        public BlogPostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View(_postService.GetAllPosts());
+        }
+
+        [HttpGet]
+        public IActionResult AdminPostEdit(int id)
+        {
+           
+            return View(_postService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult AdminPostEdit(Post post)
+        {
+            if(ModelState.IsValid)
+            {
+                _postService.Update(post);
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+    }
+}
