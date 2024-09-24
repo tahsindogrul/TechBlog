b32f8544f96124d671ddc6cfdabf468d2a596287
tahsindogrul <tahsindogrul@icloud.com>
2024-09-24 16:38:06 +0300

    BlogPostControllera AdminPostEdit HttpGet ve HttpPost actionları yazıldı

--- TechBlog.Web/Areas/Admin/Controllers/BlogPostController.cs
+++ TechBlog.Web/Areas/Admin/Controllers/BlogPostController.cs
@@ -7,34 +7,57 @@ namespace TechBlog.Web.Areas.Admin.Controllers
     public class BlogPostController : AdminBaseController
     {
         private readonly IPostService _postService;
+        private readonly ICategoryService _categoryService;
 
-        public BlogPostController(IPostService postService)
+        public BlogPostController(IPostService postService, ICategoryService categoryService)
         {
             _postService = postService;
+            _categoryService = categoryService;
         }
 
         public IActionResult Index()
         {
            
             return View(_postService.GetAllPosts());
+            
         }
 
         [HttpGet]
         public IActionResult AdminPostEdit(int id)
         {
-           
-            return View(_postService.GetById(id));
+            var posts = _postService.GetById(id);
+
+            ViewBag.Categories = _categoryService.GetAll();
+            
+                
+
+            return View(posts);
         }
 
         [HttpPost]
         public IActionResult AdminPostEdit(Post post)
         {
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
-                _postService.Update(post);
-                return RedirectToAction("Index");
+                var existingPost = _postService.GetById(post.Id);
+
+                if (existingPost != null)
+                {
+                    // Sadece Title, Content ve CategoryId'yi güncelle
+                    existingPost.Title = post.Title;
+                    existingPost.Content = post.Content;
+                    existingPost.CategoryId = post.CategoryId;
+
+                    // UserId'yi değiştirmiyoruz
+
+                    _postService.Update(existingPost);
+                    return Json(new { success = true });
+                }
             }
-            return View(post);
+            var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage);
+            return Json(new { success = false, message = "ModelState errors: " + string.Join(", ", errors) });
+
         }
     }
 }
