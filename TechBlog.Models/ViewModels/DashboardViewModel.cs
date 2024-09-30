98d1ed754478c2422d7866f284dfdc56b3276ddb
tahsindogrul <tahsindogrul@icloud.com>
2024-10-01 02:06:58 +0300

    dashboardViewModel oluşturuldu

---
+++ TechBlog.Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+         public int TotalCategories { get; set; }
+        public int PendingPosts {  get; set; }
+        public int TotalUsers { get; set; }
+        public int TotalPosts { get; set; }
+
+    }
+}
