436847f934c225d99a97019ef82e87546e6ed367
tahsindogrul <tahsindogrul@icloud.com>
2024-10-01 02:10:27 +0300

    PopularPostsViewComponent oluşturuldu.

---
+++ TechBlog.Web/Components/Posts/PopularPostsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechBlog.Business.Abstract;
+
+namespace TechBlog.Web.Components.Posts
+{
+    public class PopularPostsViewComponent :ViewComponent
+    {
+        private readonly IPostService _postService;
+
+        public PopularPostsViewComponent(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var popularPosts=  _postService.GetPopularPosts();
+            return View(popularPosts);
+        }
+    }
+}
