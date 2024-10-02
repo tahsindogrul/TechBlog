95c4364694bcd128af0ef7dec8136d2669dd92bb
tahsindogrul <tahsindogrul@icloud.com>
2024-10-02 23:34:51 +0300

    Post Modeline Photourl sütunu eklendi

---
+++ TechBlog.Data/Migrations/20241002140504_PostModelAddedPhotoUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechBlog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostModelAddedPhotoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Posts");
+        }
+    }
+}
