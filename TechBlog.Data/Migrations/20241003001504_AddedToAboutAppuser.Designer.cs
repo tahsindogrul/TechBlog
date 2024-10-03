b9d2404a89171640a6e1a85c9303fd13cdd6594e
tahsindogrul <tahsindogrul@icloud.com>
2024-10-03 23:55:02 +0300

    AppUser modeline about propertysi eklendi

--- TechBlog.Data/Migrations/20241003001504_AddedToAboutAppuser.Designer.cs
+++ TechBlog.Data/Migrations/20241003001504_AddedToAboutAppuser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechBlog.Data;
 
@@ -11,9 +12,11 @@ using TechBlog.Data;
 namespace TechBlog.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241003001504_AddedToAboutAppuser")]
+    partial class AddedToAboutAppuser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -31,6 +34,7 @@ namespace TechBlog.Data.Migrations
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
                     b.Property<string>("About")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("DateCreated")
---
+++ TechBlog.Data/Migrations/20241003001504_AddedToAboutAppuser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechBlog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedToAboutAppuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "Users");
+        }
+    }
+}
