3dd2010667d90257ea28ea3974cd3e71a7ea9322
tahsindogrul <tahsindogrul@icloud.com>
2024-10-03 23:58:27 +0300

    RegisterViewModel oluşturuldu.

---
+++ TechBlog.Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlog.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage =" Passwords don't match")]
+        public string ConfirmPassword { get; set;}
+    }
+}
